62b4935e3acd4cd0cbceb876072fe8632d6e1363
Peter Tse <tsehonam@gmail.com>
2017-08-31 01:50:27 +0800

    Added an exception.Catcher object for catching exceptions at ProcalPaser class

---
+++ src/exceptions/Catcher.kt
@@ -0,0 +1,14 @@
+package exceptions
+
+/**
+ * Exception catcher object
+ *
+ * To catch exception at ProcalParser main class,
+ * and do certain procedures for different kinds of exception caught.
+ */
+object Catcher {
+    //TODO: Implementation
+    fun catch(e: Exception) {
+        /* ... */
+    }
+}
